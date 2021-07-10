72c10462d08392b6b5a6b4ee3825479cd3537c6b
Andrew <korablinap@gmail.com>
2021-07-10 16:16:09 +0300

    Update MainWindows.cs
    Добавлен функционал кнопки дешифрования, подправлен функционал кнопки шифрования

--- MainWindows.cs
+++ MainWindows.cs
@@ -47,13 +47,12 @@ public partial class MainWindow : Gtk.Window
         }
         return;
     }
-
+    public uint shift = 14;
     protected void OnBtnCipherClicked(object sender, EventArgs e)
     {
         Random rnd = new Random();
         string s = ""; // строка шифрования
         string result = ""; // строка результата
-        int shift = rnd.Next(1,100); // выбор рандомного шага шифрования
         //Если величина сдвига больше длины алфавита кирилицы
         if (shift > 32)
             shift = shift % 32;
@@ -115,5 +114,71 @@ public partial class MainWindow : Gtk.Window
         string path = SelectedSavePath + "Ciphered.txt";
         File.Create("Ciphered.txt");
         File.WriteAllText(path, result);
+        btnCipher.Sensitive = false;
+        btnDecipher.Sensitive = false;
     }
-}
+
+    protected void OnBtnDecipherClicked(object sender, EventArgs e)
+    {
+        status.Text = "Строка считывается из файла...";
+        StreamReader sr = new StreamReader(SelectedFilePath);
+        string s = sr.ReadToEnd();
+        string result = "";
+        sr.Close();
+        //Цикл по каждому символу строки
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (Convert.ToInt16(s[i]) == 32)
+                result += ' ';
+            //Если буква является строчной
+            if ((Convert.ToInt16(s[i]) >= 1072) && (Convert.ToInt16(s[i]) <= 1103))
+            {
+                //Если буква, после сдвига выходит за пределы алфавита
+                if (Convert.ToInt16(s[i]) - shift < 1072)
+                    //Добавление в строку результатов символ
+                    result += Convert.ToChar(Convert.ToInt16(s[i]) - shift + 32);
+                //Если буква может быть сдвинута в пределах алфавита
+                else
+                    //Добавление в строку результатов символ
+                    result += Convert.ToChar(Convert.ToInt16(s[i]) - shift);
+            }
+            //Если буква является прописной
+            if ((Convert.ToInt16(s[i]) >= 1040) && (Convert.ToInt16(s[i]) <= 1071))
+            {
+                //Если буква, после сдвига выходит за пределы алфавита
+                if (Convert.ToInt16(s[i]) - shift < 1040)
+                    //Добавление в строку результатов символ
+                    result += Convert.ToChar(Convert.ToInt16(s[i]) - shift + 32);
+                //Если буква может быть сдвинута в пределах алфавита
+                else
+                    //Добавление в строку результатов символ
+                    result += Convert.ToChar(Convert.ToInt16(s[i]) - shift);
+            }
+        }
+        status.Text = "Строка дешифрована успешно";
+        using (FileChooserDialog d =
+        new FileChooserDialog(null,
+                              "Выберите путь сохранения...",
+                              null,
+                              FileChooserAction.Open,
+                              "Выбрать",
+                              ResponseType.Accept,
+                              "Закрыть",
+                              ResponseType.Close))
+        {
+            if (d.Run() == (int)ResponseType.Accept)
+            {
+                SelectedSavePath = d.Filename;
+                d.Destroy();
+            }
+            else if (d.Run() == (int)ResponseType.Close)
+            {
+                d.Destroy();
+            }
+        }
+        string path = SelectedSavePath + "Deciphered.txt";
+        File.Create("Deciphered.txt");
+        File.WriteAllText(path, result);
+        btnCipher.Sensitive = false;
+        btnDecipher.Sensitive = false;
+    }
